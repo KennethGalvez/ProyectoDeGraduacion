9904ffa82b9d6a612b6a835103274e4fa53c6efd
KennethGalvez <60400757+KennethGalvez@users.noreply.github.com>
2024-04-29 17:48:19 -0600

    Animaciones Salto + Correr

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,10 @@ public class PlayerMovement : MonoBehaviour
     public float fallMultiplier = 0.5f;
 
     public float lowJumpMultiplier = 1f;
+
+    public SpriteRenderer spriteRenderer;
+
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,19 +30,35 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             rb2D.velocity = new Vector2(runSpeed,rb2D.velocity.y);
+            spriteRenderer.flipX = false;
+            animator.SetBool("Run", true);
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
             rb2D.velocity = new Vector2(-runSpeed,rb2D.velocity.y);
+            spriteRenderer.flipX = true;
+            animator.SetBool("Run", true);
         }
         else
         {
             rb2D.velocity = new Vector2 (0, rb2D.velocity.y);
+            animator.SetBool("Run", false);
         }
+        
         if (Input.GetKey("space") && CheckGround.isGrounded)
         {
             rb2D.velocity = new Vector2(rb2D.velocity.x , jumpSpeed);
         }
+        if (CheckGround.isGrounded == false)
+        {
+            animator.SetBool ("Jump", true);
+            animator.SetBool("Run", false);
+        }
+        if (CheckGround.isGrounded == true)
+        {
+            animator.SetBool ("Jump", false);
+        }
+
         if (betterJump)
         {
             if (rb2D.velocity.y < 0)
